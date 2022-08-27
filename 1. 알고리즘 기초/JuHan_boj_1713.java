521ae9761ce5abff9c647113abf26d71e840b25f
ParkJuHan <1004sunye@naver.com>
2022-08-28 04:45:33 +0900

    구현 + 시뮬레이션

--- 1. 알고리즘 기초/JuHan_boj_1713.java
+++ 1. 알고리즘 기초/JuHan_boj_1713.java
@@ -1,58 +1,103 @@
+//package DAY02.P1713;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static ArrayList<picture> array = new ArrayList<>();
 
-        int total = 0;
-        int now = 0;
-        int candidateIdx = 0;         //후보가 들어갈 사진틀 위치
-        int pictureNum =Integer.parseInt(br.readLine());        //사진틀 개수
-        StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+       // System.setIn(new FileInputStream("src/DAY02/P1713/input.txt"));
 
-        int[] student = new int[pictureNum];
-        int[] recommend = new int[pictureNum];      //추천 횟수
-        int[] time = new int[pictureNum];           //추천 시간
+        BufferedReader br = br = new BufferedReader(new InputStreamReader(System.in));
 
-        total = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-        for (int x = 0; x < total ; x++){
+        picture tmp;
 
-            now =  Integer.parseInt((st.nextToken()));      
-            candidateIdx=0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int y = 0 ; y < pictureNum ; y++){
-                //사진틀이 비었거나, 이미 게시된 경우
-                if( student[y] == 0 | student[y] == now ) {
-                    candidateIdx = y;
-                    break;
+        while(st.hasMoreTokens()){
+            int inputNum = Integer.parseInt(st.nextToken());
+
+            if(array.size() == N){
+                int flag = 0;
+                int idx = 0;
+
+                //중복 검사
+                for(int i = 0 ; i < N; i++){
+                    if(array.get(i).num == inputNum) {
+                        flag = 1;
+                        idx = i;
+                        break;
+                    }
                 }
-               //추천 횟수가 더 적거나, 추천횟수가 같고 시간이 더 빠른 경우
-                if( recommend[candidateIdx] > recommend[y] || recommend[candidateIdx] == recommend[y] && time[candidateIdx] > time[y]) {
-                    candidateIdx = y ;
+                if (flag == 1) {
+                    //꽉 찼는데 중복이 있을 때
+                    array.get(idx).like++;
+                }else{
+                    //꽉 찼는데 중복이 없을 때 -> 삭제하고 넣어야 돼
+                    int min = (int)1e9;
+                    int minIdx = 0;
+
+                    //삭제할 애 찾기 -> minIdx 찾기
+                    for(int j = N - 1; j >= 0; j--){
+                        if(array.get(j).like <= min){
+                            min = array.get(j).like;
+                            minIdx = j;
+                        }
+                    }
+
+                    //삭제하고 끼워넣기
+                    array.remove(minIdx);
+                    array.add(new picture(inputNum, 1));
+                }
+            }else{
+                //사진틀이 꽉 안 찼을때
+                int flag = 0;
+                int idx = 0;
+
+                if(array.size() == 0){
+                    array.add(new picture(inputNum, 1));
+                }else{
+                    //중복 검사
+                    for(int i = 0 ; i < array.size(); i++) {
+                        if(array.get(i).num == inputNum){
+                            flag = 1;
+                            idx = i;
+                            break;
+                        }
+                    }
+
+                    if(flag == 1){
+                        array.get(idx).like++;
+                    } else{
+                        array.add(new picture(inputNum, 1));
+                    }
                 }
             }
-
-            if(student [candidateIdx] != now){
-                student[candidateIdx] = now;
-                recommend[candidateIdx] = 0;
-                time[candidateIdx] = x;
-            }
-                recommend[candidateIdx]++;
         }
 
-        Arrays.sort(student);
-
-        for (int x : student){
-            if(x!=0){
-                bw.write(String.valueOf(x)+"");
-            }
+        Collections.sort(array);
+        for(int i = 0; i < array.size(); i++){
+            System.out.println(array.get(i).num);
         }
-        bw.flush();
     }
 }
 
+class picture implements Comparable<picture>{
+    int num = 0;
+    int like = 0;
 
+    public picture(int num, int like) {
+        this.num = num;
+        this.like = like;
+    }
+
+    @Override
+    public int compareTo(picture o) {
+        return num - o.num;
+    }
+}
