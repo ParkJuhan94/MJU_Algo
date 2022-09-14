c514bf844a1cc814e46dbcb55fb6fbe9d73ad33e
ParkJuHan <1004sunye@naver.com>
2022-09-15 00:09:03 +0900

    [그리디] 어려웠음

--- 1. 알고리즘 기초/1-1. 문자열, 그리디/JuHan_boj_1246.java
+++ 1. 알고리즘 기초/1-1. 문자열, 그리디/JuHan_boj_1246.java
@@ -33,8 +33,9 @@ public class Main {
         Collections.sort(arr);
 
         //계란 가격을 낮춰가면서 살 손님이 한 명 더 생기는 순간, 이익을 비교
-        //-> 여기서 더 손해라고 해서 바로 break 하면 안 돼!!!!!!!
-        //낮은 금액의 구매자가 1000명 일 수도 있잖아.
+        //계란 가격을 1씩 낮추지 않고, 가격 배열에서 뽑아와서 낮춤 -> 시간복잡도 low
+        //다음 loof에서 더 손해가 나왔더라도 break 하면 안 돼!!!!
+        //그 다음 루프에서의 구매자가 30000명 일 수도 있잖아.
         for(numEgg = 1; numEgg < M + 1; numEgg++){
             price = arr.get(M - numEgg);
             sum = price * numEgg;
