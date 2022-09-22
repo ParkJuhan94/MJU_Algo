8dcd817fcb3cf99f7d8201a26a3186ebcc45a023
ParkJuHan <1004sunye@naver.com>
2022-09-22 21:16:34 +0900

    Create JuHan_boj_7576.java

---
+++ 1. 알고리즘 기초/1-3. 재귀, DFS, BFS/JuHan_boj_7576.java
@@ -0,0 +1,51 @@
+package WEEK04.P7576;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, res;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static Queue<Integer> Q;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/WEEK04/P7576/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        BFS();
+        
+        System.out.println(res);
+    }
+
+    static void BFS(){
+        Q = new LinkedList<>();
+        
+        while(!Q.isEmpty()){
+            int head = Q.poll();
+            
+            
+            
+            
+        }
+    }
+}
