50980c07d324c83516e9403f7a413060c2d9eca5
ParkJuHan <1004sunye@naver.com>
2022-10-16 02:46:40 +0900

    Update JuHan_boj_1246.java

--- 1. 알고리즘 기초/1-1. 문자열, 그리디/JuHan_boj_1246.java
+++ 1. 알고리즘 기초/1-1. 문자열, 그리디/JuHan_boj_1246.java
@@ -1,4 +1,3 @@
-
 package WEEK01.P1246;
 
 import java.io.BufferedReader;
@@ -12,7 +11,7 @@ import java.util.StringTokenizer;
 public class Main {
     static int N,M;
     static int orgSum = 0, sum, numEgg, price, salePrice, max = 0;
-    static ArrayList<Integer> arr;
+    static ArrayList<Integer> pay;
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/WEEK01/P1246/input.txt"));
@@ -22,22 +21,23 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new ArrayList<>();
+        pay = new ArrayList<>();    // 각자의 고객이 달걀 하나에 지불 가능한 최대의 가격
+
 
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int input = Integer.parseInt(st.nextToken());
-            arr.add(input);
+            pay.add(input);
         }
 
-        Collections.sort(arr);
+        Collections.sort(pay);
 
-        //계란 가격을 낮춰가면서 살 손님이 한 명 더 생기는 순간, 이익을 비교
-        //계란 가격을 1씩 낮추지 않고, 가격 배열에서 뽑아와서 낮춤 -> 시간복잡도 low
-        //다음 loof에서 더 손해가 나왔더라도 break 하면 안 돼!!!!
-        //그 다음 루프에서의 구매자가 30000명 일 수도 있잖아.
+        // 계란 가격을 낮춰가면서 살 손님이 한 명 더 생기는 순간, 이익을 비교
+        // -> 여기서 더 손해라고 해서 바로 break 하면 안 돼!!!!!!!
+        // 낮은 금액의 구매자가 1000명 일 수도 있잖아.
         for(numEgg = 1; numEgg < M + 1; numEgg++){
-            price = arr.get(M - numEgg);
+            // 고객들의 지불 가격을 기준으로 함 . 그 중간으로 해봤자 이익이 최대가 될 수 없으니까
+            price = pay.get(M - numEgg);
             sum = price * numEgg;
 
             if(sum > max){
